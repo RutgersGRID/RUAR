eebb4e069848902b8d2f0761793890d3bd64e702
Rick Anderson <rianders@docs.rutgers.edu>
2016-09-23 15:02:10 -0400

    Updated for Boyd Park Imagery.

---
+++ Assets/PosChildFromParent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class PosChildFromParent : MonoBehaviour {
+	public GameObject ChildObject;
+
+	// Use this for initialization
+	void Start () {
+		//this.transform.localPosition=Vector3.zero; 
+		//ChildObject = transform.GetChild(0);
+		//this.transform.GetChild(0).localPosition 
+		//this.transform.GetChild(0).localPosition = new Vector3 (-0.25f,0.15f,0.0f);
+		//Debug.Log(this.transform.GetChild (0).name);
+		//ChildObject.localPosition = new Vector3 (-0.25f,0.15f,0.0f);
+		Transform[] allChildren = GetComponentsInChildren<Transform>();
+		foreach (Transform child in allChildren) {
+			// do whatever with child transform here
+			child.transform.localPosition = Vector3.zero;
+			//child.localPosition = new Vector3 (-0.25f,0.15f,0.0f);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/PosChildToParent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class PosChildToParent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//this.transform.localPosition=Vector3.zero; 
+		this.transform.localPosition = new Vector3 (-0.25f,0.15f,0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
