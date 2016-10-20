37e94b3262767997b7ec6170003dc1a75b8ec977
Rick Anderson <rianders@docs.rutgers.edu>
2016-10-20 11:38:58 -0400

    added title screen and touch loader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			//Debug.Log ("A key or mouse click has been detected");
+			SceneManager.LoadScene (1);
+		}
+	}
+}
